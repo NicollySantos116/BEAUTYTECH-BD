715c4a39cbfde4d7a57850eaf95e5d00537afcdd
Nicolly <saantos.nick1606@gmail.com>
2025-09-22 10:46:05 -0300

    feat: corrigindo e documentando a criação do banco beautytechdb

--- queries/01-insert-catalogo.sql
+++ queries/01-insert-catalogo.sql
@@ -1,3 +1,16 @@
+CREATE DATABASE beautytechdb;
+
+\c beautytechdb 
+
+CREATE TABLE produtos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    marca VARCHAR(50) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    categoria VARCHAR(30) NOT NULL,
+    estoque INT DEFAULT 0
+);
+
 INSERT INTO produtos (nome, marca, preco, categoria, estoque) 
 VALUES
 ('Niacinamide 10% + Zinc 1%', 'The Ordinary', 59.90, 'skincare', 25),
