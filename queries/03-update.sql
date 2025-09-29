e78cf7c4533cc14b14ca4af421b253910711a049
Nicolly Santos <saantos.nick1606@gmail.com>
2025-09-29 14:05:33 -0300

    feat: atividade corrigida

--- queries/03-update.sql
+++ queries/03-update.sql
@@ -3,27 +3,26 @@ WHERE categoria = 'skincare';
 
 SELECT * FROM produtos;
 
-UPDATE produtos 
-SET preco = preco * 1.15
+UPDATE produtos SET preco = preco * 1.15
 WHERE marca = 'Glossier';
 
 SELECT * FROM produtos;
+UPDATE produtos SET estoque = estoque + 50
 
-UPDATE produtos
-SET preco = preco * 0.9
+SELECT * FROM produtos;
+
+UPDATE produtos SET preco = preco * 0.9
 WHERE estoque > 100;
 
 SELECT * FROM produtos;
 
 ---  Desafio ---
-UPDATE produtos
-SET preco = preco * 0.7
+UPDATE produtos SET preco = preco * 0.7
 WHERE categoria = 'perfumes';
 
 SELECT * FROM produtos;
 
-UPDATE produtos 
-SET estoque = estoque + 25
+UPDATE produtos SET estoque = estoque + 25
 WHERE preco > 150.00
 
 SELECT * FROM produtos;
